877203fbe3ae2851d5523bf5baaca7bc476d9e7b
mosin <ryoctrl@gmail.com>
2019-04-14 04:36:04 +0900

    adjust cart design

--- src/components/CartModal.js
+++ src/components/CartModal.js
@@ -1,40 +1,50 @@
 import React, { Component } from 'react';
-import { Dialog, DialogContent, DialogTitle } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import { Dialog, DialogContent, DialogTitle, Avatar, List, ListItem, ListItemAvatar, ListItemText } from '@material-ui/core';
 import CartContext from '../contexts/cart';
+
+const IMAGE_PATH = process.env.REACT_APP_API_HOST + 'images/';
 
 class CartModal extends Component {
     render() {
-        const { open, onClose } = this.props;
-        return (
-            <Dialog open={open} onClose={() => onClose()} aria-labelledby="form-dialog-title">
-                <DialogTitle id="form-dialog-title">Cart!!</DialogTitle>
-                <CartContext.Consumer>
-                    {context => 
-                            context.cart.map(item => {
-                                return (
-                                    <DialogContent key={item.id}>
-                                        {item.name} - {item.amount}
-                                    </DialogContent>
-                                )
+        const { classes, open, onClose } = this.props;
+                return (
+            <Dialog open={open} onClose={() => onClose()} aria-labelledby="form-dialog-title" className={classes.dialog}>
+                <DialogTitle id="form-dialog-title">ShoppingCart</DialogTitle>
+                <List>
+                    <CartContext.Consumer>
+                        {context => 
+                                context.cart.map(item => {
+                                    let img;
+                                    if(!item.image) img = '/img/no-image.svg';
+                                    else img = IMAGE_PATH + item.image;
+                                    return (
+                                        <ListItem key={item.id}>
+                                            <ListItemAvatar>
+                                                <Avatar alt={item.name} src={img} className={classes.avatar}/>
+                                            </ListItemAvatar>
+                                            <ListItemText primary={item.name} secondary={item.amount} />
+                                        </ListItem>
+                                    )
 
-                            })
-                    }
-                </CartContext.Consumer>
+                                })
+                        }
+                    </CartContext.Consumer>
+                </List>
             </Dialog>
         )
     }
 }
 
-export default CartModal;
-
-/*
 const styles = theme => ({
-    textField: {
-        marginLeft: theme.spacing.unit,
-        marginRight: theme.spacing.unit,
-        width: 200,
+    avatar: {
+        display: 'inline-block',
+        width: 40,
+        height: 40
+    },
+    info: {
+        lineHeight: '40px',
     }
 });
 
 export default withStyles(styles)(CartModal);
-*/
