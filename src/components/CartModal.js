1c3bb43e4e3e1d98b35d8db3f1579866f2e7340e
mosin <ryoctrl@gmail.com>
2019-04-19 05:18:45 +0900

    Don't use context store and use redux stores

--- src/components/CartModal.js
+++ src/components/CartModal.js
@@ -1,38 +1,35 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import { Dialog, DialogContent, DialogTitle, Avatar, List, ListItem, ListItemAvatar, ListItemText } from '@material-ui/core';
-import CartContext from '../contexts/cart';
+import { connect } from 'react-redux';
 
 const IMAGE_PATH = process.env.REACT_APP_API_HOST + 'images/';
 
 class CartModal extends Component {
     render() {
-        const { classes, open, onClose } = this.props;
-                return (
-            <Dialog open={open} onClose={() => onClose()} aria-labelledby="form-dialog-title" className={classes.dialog}>
+        const { classes, open, onClose, cart } = this.props;
+
+        return (
+            <Dialog open={open} onClose={onClose} aria-labelledby="form-dialog-title" className={classes.dialog}>
                 <DialogTitle id="form-dialog-title">ShoppingCart</DialogTitle>
                 <List>
-                    <CartContext.Consumer>
-                        {context => 
-                                context.cart.map(item => {
-                                    let img;
-                                    if(!item.image) img = '/img/no-image.svg';
-                                    else img = IMAGE_PATH + item.image;
-                                    return (
-                                        <ListItem key={item.id}>
-                                            <ListItemAvatar>
-                                                <Avatar alt={item.name} src={img} className={classes.avatar}/>
-                                            </ListItemAvatar>
-                                            <ListItemText primary={item.name} secondary={item.amount} />
-                                        </ListItem>
-                                    )
+                    { cart.map(item => {
+                        let img;
+                        if(!item.image) img = '/img/no-image.svg';
+                        else img = IMAGE_PATH + item.image;
+                        return (
+                            <ListItem key={item.id}>
+                                <ListItemAvatar>
+                                    <Avatar alt={item.name} src={img} className={classes.avatar}/>
+                                </ListItemAvatar>
+                                <ListItemText primary={item.name} secondary={item.amount} />
+                            </ListItem>
+                        )
+                    })}
 
-                                })
-                        }
-                    </CartContext.Consumer>
                 </List>
             </Dialog>
-        )
+        );
     }
 }
 
@@ -47,4 +44,7 @@ const styles = theme => ({
     }
 });
 
-export default withStyles(styles)(CartModal);
+CartModal = connect(s => s)(CartModal);
+CartModal = withStyles(styles)(CartModal);
+
+export default CartModal;
