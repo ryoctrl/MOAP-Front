9022c8ec003a60c286152806ad16cebc14639c70
ryoctrl@gmail.com <ryoctrl@gmail.com>
2019-07-24 06:47:03 +0900

    improve menues ux

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Grid } from '@material-ui/core';
 import { connect } from 'react-redux';
+import { Bounce } from 'react-activity';
+import 'react-activity/dist/react-activity.css';
 import { withStyles } from '@material-ui/core/styles';
 import MenuCard from '../components/MenuCard';
 
@@ -12,37 +14,32 @@ class Menu extends Component {
         const { classes, menu } = this.props;
         const menuList = menu.list;
         return (
-            <Grid container spacing={24}>
-            {menuList.map(item => (
-                <Grid key={item.id} item xs={6} lg={3} className={classes.card}>
-                    <MenuCard key={item.id} menu={item}/>
-                </Grid>
-            ))}
-            </Grid>
-        )
-
-        /*
-        const { error, isLoaded, menu } = this.state;
-        if(error) {
-            return <div> Error: {error.message}</div>
-        } else if(!isLoaded) {
-            return <div> Loading... </div>;
-        } else {
-            return (
+            <div className={classes.container}>
+                {menu.fetching && <Bounce className={classes.loader}/>}
+                {!menu.fetching && menuList.length === 0 && <div>{menu.error}</div>}
                 <Grid container spacing={24}>
-                {menu.map(item => (
+                {menuList.map(item => (
                     <Grid key={item.id} item xs={6} lg={3} className={classes.card}>
                         <MenuCard key={item.id} menu={item}/>
                     </Grid>
                 ))}
                 </Grid>
-            )
-        }
-        */
+            </div>
+        )
     }
 }
 
 const styles = theme => ({
+    loader: {
+        height: '8px',
+        width: '30px',
+        position: 'absolute',
+        margin: 'auto',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+    },
     card: {
         width: '100%',
 
