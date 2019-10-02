82fa63f4999a2b00d9951377b750817fe1f2bfec
ryoctrl@gmail.com <ryoctrl@gmail.com>
2019-10-03 04:49:00 +0900

    implement payment function

--- src/libs/nem.js
+++ src/libs/nem.js
@@ -21,16 +21,17 @@ const host = new TransactionHttp(NEM_NODE_HOST);
 */
 
 //店舗のNEMアドレス(売上の受取)
-const STORE_NEM_ADDR = 'SBS2C4TXH3EULCD3X24MYCR7HQTDAGRMLRPKGFDN';
+const STORE_NEM_ADDR = 'SCXQVSLDPTOP7OBS7EW454ZQQTXJRTV7V3MQGAYA';
 //moapで使用するMOSAICのID
-const MOSAIC_ID = '0bf26981a0ed862d';
+const MOSAIC_ID = '2bb09db8269361d6';
 //使用するnem chainのnetwork_generation_hash
-const NETWORK_GENERATION_HASH = '453052FDC4EB23BF0D7280C103F7797133A633B68A81986165B76FCE248AB235';
+const NETWORK_GENERATION_HASH = '249B14C178A3E7A2C8556EC3571FFAD4BAB2E71349FFD6E97A06675906EA1584';
 // nem node
-const NEM_NODE_HOST = 'https://catapult-test.opening-line.jp:3001/';
+//const NEM_NODE_HOST = 'https://catapult-test.opening-line.jp:3001/';
+const NEM_NODE_HOST = 'https://nemp2p.mosin.jp';
 const host = new TransactionHttp(NEM_NODE_HOST);
 
-export default (amount, privateKey) => {
+export default async (amount, privateKey) => {
     console.log(`sending ${amount} ${privateKey}`);
     const transaction = TransferTransaction.create(
         Deadline.create(),
@@ -48,8 +49,15 @@ export default (amount, privateKey) => {
     const sender = Account.createFromPrivateKey(privateKey, NetworkType.MIJIN_TEST);
     const signedTransaction = sender.sign(transaction, NETWORK_GENERATION_HASH);
 
-    host.announce(signedTransaction)
-        .subscribe(console.log, console.error);
+    const res = await new Promise((resolve, reject) => {
+        console.log('announcing transaction!');
+        const succeeded = transactions => resolve(transactions);
+        const failured = err => reject(err);
+        host.announce(signedTransaction).subscribe(succeeded, failured);
+    }).catch(err => console.error(err));
+    console.log('announced!');
+    console.log(res);
+    return { data: true };
 };
 
 
